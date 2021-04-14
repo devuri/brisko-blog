654f2b5c845a769836392f3a9cc7c7f8cad74fdd
devuri <devuriko@gmail.com>
2021-04-14 02:51:42 -0500

    cs fix

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@
  *
  * @package brisko
  */
-
 function brisko_blog_google_fonts() {
 
 	wp_enqueue_style( 'brisko-blog-font', 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800', false );
